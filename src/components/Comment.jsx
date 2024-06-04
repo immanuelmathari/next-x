8cb2ccfdb51cac194f77b43f53479274d6db7a58
Evil Genius <immanuelnganga2001@gmail.com>
2024-06-04 22:06:15 +0300

    add like comment. done with project

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,49 @@
 // for each comment
 "use client"
-import React from 'react'
-import { HiDotsHorizontal } from 'react-icons/hi'
+import { app } from '@/firebase';
+import { collection, deleteDoc, doc, getFirestore, onSnapshot, serverTimestamp, setDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { HiDotsHorizontal, HiHeart, HiOutlineHeart } from 'react-icons/hi'
+import { signIn, useSession } from 'next-auth/react';
 
-export default function Comment({comment, id}) {
+export default function Comment({comment, commentId, originalPostId}) {
+    const [isLiked, setIsLiked] = useState(false);
+    const[likes, setLikes] = useState([]);
+    const db = getFirestore(app);
+    const {data:session} = useSession();
+
+    useEffect(() => {
+        // here we are saying we go to the comments of the post and wich comments are we going to the ones that have likes
+        onSnapshot(collection(db, 'posts' , originalPostId, 'comments', commentId , 'likes'), (snapshot) => {
+            // all the likes
+            setLikes(snapshot.docs)
+        });
+    }, [db]);
+
+    useEffect(() => {
+        // check each like and check if its equal to our id
+        setIsLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1);
+    }, [likes]);
+    
+    const likePost = async () => {
+        if(session){
+            if(isLiked){
+                // here after we go to comments, we create a new collection called likes
+                await deleteDoc(doc(db, 'posts', originalPostId, 'comments', commentId , 'likes' , session?.user.uid))
+            } else {
+                // likes is the name of the new collection and we add the user id
+                await setDoc(doc(db, 'posts' , originalPostId, 'comments', commentId , 'likes' , session.user.uid), {
+                    username: session.user.username,
+                    timestamp: serverTimestamp(),
+                });
+                
+            }
+        } else {
+            // go to sign in
+            signIn();
+        }
+    };
+
   return (
     <div className='flex p-3 border-b border-gray-200 hover:bg-gray-50 pl-10'>
       {/* This ? is optional chaining operator. It checks 
@@ -18,6 +58,19 @@ export default function Comment({comment, id}) {
         <HiDotsHorizontal className='text-sm' />
         </div>
         <p className='text-gray-800 text-xs my-3'>{comment?.comment}</p>
+        
+        <div className='flex items-center'>
+        {isLiked ? (
+                    <HiHeart onClick={likePost} className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100' />
+
+        ) : (
+            <HiOutlineHeart onClick={likePost} className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
+
+        )}
+        {likes.length > 0 && <span className={`text-xs ${isLiked && "text-red-600"}`}>{likes.length}</span>}
+        </div>
+
+
       </div>
     </div>
   )
