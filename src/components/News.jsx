e492afc11b81378b5733f49c3e7b910512657023
Evil Genius <immanuelnganga2001@gmail.com>
2024-11-23 21:58:48 +0300

    updating news component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -14,14 +14,15 @@ export default function News() {
     <div className='text-gray-700 space-y-1 bg-gray-100 rounded-xl pt-2'>
         <h4 className='font-bold text-xl px-4'>Whats happening</h4>
         {news.slice(0, articleNumber).map((article) => (
+            // obviously when mapping through something you need a key
             <div className='' key={article.url}>
                 <a href={article.url} target='_blank'>
-                <div className='flex items-center justify-between px-4 py-2 space-x-1'>
-                    <div>
+                <div className='flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200'>
+                    <div className='space-y-0.5'>
                         <h6 className='text-sm font-bold'>{article.title}</h6>
                         <p className='text-xs font-medium text-gray-500'>{article.source.name}</p>
                         </div>
-                        <img src={article.urlToImage} alt={article.title} width={70} className='rounded-xl' />
+                        <img src={article.urlToImage} width={70} className='rounded-xl' />
                     </div>
                     </a>
             </div>
